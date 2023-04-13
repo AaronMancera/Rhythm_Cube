630bd784eea04d0a544f9b55cb81c6ce507a2163
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-04-13 12:08:57 +0200

    [ Login_Register ]
    Creacion de los paneles necesarios para el resgistro y usuarios

--- Assets/Scripts/FirebaseController.cs
+++ Assets/Scripts/FirebaseController.cs
@@ -9,15 +9,19 @@ using TMPro;
 public class FirebaseController : MonoBehaviour
 {
     //Objetos del canvas
-    public GameObject loginPanel, singupPanel, profilePanel, forgetPasswordPanel;
+    public GameObject loginPanel, singupPanel, profilePanel, forgetPasswordPanel, notificationPanel;
     //TextField
     public TMP_InputField loginEmail, loginPassword, singupEmail, singupPassword, singupCPasswprd, singupUsername, forgetPassEmail;
+    //Text
+    public TMP_Text notif_Title_Text, notif_Message_Text, profileUserName_Text, profileEmail_Text;
+    //Toggle
+    public Toggle remeberMe;
     //Control de que paneles mostrar en el canvas
     public void OpenLoginPanel()
     {
         loginPanel.SetActive(true);
         singupPanel.SetActive(false);
-        singupPanel.SetActive(false);
+        profilePanel.SetActive(false);
         forgetPasswordPanel.SetActive(false);
     }
 
@@ -25,7 +29,7 @@ public class FirebaseController : MonoBehaviour
     {
         loginPanel.SetActive(false);
         singupPanel.SetActive(true);
-        singupPanel.SetActive(false);
+        profilePanel.SetActive(false);
         forgetPasswordPanel.SetActive(false);
     }
 
@@ -33,7 +37,7 @@ public class FirebaseController : MonoBehaviour
     {
         loginPanel.SetActive(false);
         singupPanel.SetActive(false);
-        singupPanel.SetActive(true);
+        profilePanel.SetActive(true);
         forgetPasswordPanel.SetActive(false);
     }
 
@@ -41,7 +45,7 @@ public class FirebaseController : MonoBehaviour
     {
         loginPanel.SetActive(false);
         singupPanel.SetActive(false);
-        singupPanel.SetActive(false);
+        profilePanel.SetActive(false);
         forgetPasswordPanel.SetActive(true);
     }
 
@@ -50,15 +54,24 @@ public class FirebaseController : MonoBehaviour
     {
         if(string.IsNullOrEmpty(loginEmail.text)&&string.IsNullOrEmpty(loginPassword.text))
         {
+            showNotificationMessage("Error", "Fields Empty!\nPlease Input All Details");
             return;
         }
         //TODO: Hacer el login
+    }
+    //Cerrar sesion
+    public void LogOut()
+    {
+        profileUserName_Text.text="";
+        profileEmail_Text.text="";
+        OpenLoginPanel();
     }
     //Registro
     public void SingUpUser()
     {
         if(string.IsNullOrEmpty(singupEmail.text)&&string.IsNullOrEmpty(singupPassword.text)&&string.IsNullOrEmpty(singupCPasswprd.text))
         {
+            showNotificationMessage("Error", "Fields Empty!\nPlease Input All Details");
             return;
         }
         //TODO: Hacer el registro
@@ -68,15 +81,32 @@ public class FirebaseController : MonoBehaviour
     {
         if(string.IsNullOrEmpty(forgetPassEmail.text))
         {
+            showNotificationMessage("Error", "Forget Email Empty");
             return;
         }
         //TODO: Hacer la recuperacion de contraseña
     }
+    //Creacion de la notificacion
+    private void showNotificationMessage(string title, string message)
+    {
+        notif_Title_Text.text=""+title;
+        notif_Message_Text.text=""+message;
+        notificationPanel.SetActive(true);
+
+    }
+    //Cerrar la notificacion
+    public void CloseNotif_Panel()
+    {
+        notif_Title_Text.text="";
+        notif_Message_Text.text="";
+        notificationPanel.SetActive(false);
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
