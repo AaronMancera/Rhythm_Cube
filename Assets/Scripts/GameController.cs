7c6924441ec18ab12004440943dbffb0b847b804
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-04-24 13:34:30 +0200

    [ Sonido y UI ]
    Creacion de script controlador de la musica y creacion basica de un menu de pausa

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class GameController : MonoBehaviour
 {
-    //El obejto que va a seguir (player)
-    public GameObject player;
+    public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        audioSource.Play();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x+3, player.transform.position.y+2, transform.position.z);
-
+        //TODO: Poner una configuracion para el volumen
+        //establecer el volumen
+        audioSource.volume = 0.5f;
     }
 }
