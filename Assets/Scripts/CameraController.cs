56029ec53539da5bb327e96f6f5888a6c9677795
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-05-07 16:24:45 +0200

    [ Sprite & Correcciones ]
    Se han añadido un alias donde contiene todas las texturas actuales y animaciones aun no aplicadas, tambien se ha solucionado lo de bestScore y sobre el spawn del jugador en vez de la recarga de la escena

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,15 +11,21 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!end)
+
+        if (GameObject.FindGameObjectWithTag("Player") != null)
         {
-            transform.position = new Vector3(player.transform.position.x + 3, player.transform.position.y + 2, transform.position.z);
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            if (!end)
+            {
+                transform.position = new Vector3(player.transform.position.x + 3, player.transform.position.y + 2, transform.position.z);
+            }
         }
     }
     public void stopFollow()
